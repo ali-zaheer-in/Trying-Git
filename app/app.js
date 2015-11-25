afac0fd95206a5c86652ea8f1710f1aa4cf02431
alizaheer <syedali.zaheer@accolite.com>
2015-11-25 15:54:40 +0530

    first commit

---
+++ app/app.js
@@ -0,0 +1,17 @@
+angular.module('myapp',[])
+    .controller('Main', function() {
+    	var vm=this;
+        vm.model="aa";
+    	vm.get=get;
+        vm.parentModel="parentModel";
+    	function get(){
+    		this.name="Ali Zaheer";
+    		alert(vm.name);
+    	}
+    })
+    .controller('Child',function(){
+        var vm=this;
+        vm.ChildModel="ChildModel";
+        vm.model="Child!";
+    });
+
