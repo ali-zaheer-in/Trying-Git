e9cb0fe4de72b18f0f85c4b547253d16e2046987
alizaheer <syedali.zaheer@accolite.com>
2015-11-25 16:04:33 +0530

    3rd committ

--- app/app.js
+++ app/app.js
@@ -9,11 +9,10 @@ angular.module('myapp',[])
     		this.name="Ali Zaheer";
     		alert(vm.name);
     	}
-    })
+    })   
     .controller('Child',function(){
         var vm=this;
         vm.ChildModel="ChildModel";
         vm.model="Child!";
     });
-
 })();
